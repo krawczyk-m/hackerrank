ae7b9b021d88076b061b279e98f05e8e4c2aa9de
Michal Krawczyk <krawczyk.michal91@gmail.com>
2015-09-10 23:35:38 +0200

    Added HackerRank Extra Long Factorial solution and tests

---
+++ warmup/src/main/java/com/hackerrank/mk/ExtraLongFactorial.java
@@ -0,0 +1,29 @@
+package com.hackerrank.mk;
+
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class ExtraLongFactorial {
+
+    public static BigInteger factorial(BigInteger n) {
+        return iFactorial(n, BigInteger.ONE);
+    }
+
+    private static BigInteger iFactorial(BigInteger n, BigInteger current) {
+        if (n.equals(BigInteger.ZERO)) {
+            return current;
+        } else {
+            return iFactorial(n.subtract(BigInteger.ONE), current.multiply(n));
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        BigInteger n = in.nextBigInteger();
+
+        System.out.println(factorial(n));
+    }
+}
