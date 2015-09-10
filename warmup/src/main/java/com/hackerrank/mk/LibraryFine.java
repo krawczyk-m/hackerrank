af3fbf139385a8b30408f4aef762d096486dc286
Michal Krawczyk <krawczyk.michal91@gmail.com>
2015-09-10 23:15:26 +0200

    Add HackerRank Library Fine challenge

---
+++ warmup/src/main/java/com/hackerrank/mk/LibraryFine.java
@@ -0,0 +1,55 @@
+package com.hackerrank.mk;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Scanner;
+
+public class LibraryFine {
+
+    public static int hackosPerDay = 15;
+    public static int hackosPerMonth = 500;
+    public static int hackosYear = 10000;
+
+
+    public static int calculateLibraryFine(String sActual, String sExpected) {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("dd MM yyyy");
+        Date dActual = null;
+        Date dExpected = null;
+
+        Calendar actual = Calendar.getInstance();
+        Calendar expected = Calendar.getInstance();
+
+        try {
+            dActual = sdf.parse(sActual);
+            dExpected = sdf.parse(sExpected);
+
+            actual.setTime(dActual);
+            expected.setTime(dExpected);
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        if (actual.compareTo(expected) <= 0) {
+            return 0;
+        } else if (actual.get(Calendar.YEAR) != expected.get(Calendar.YEAR)) {
+            return hackosYear;
+        } else if (actual.get(Calendar.MONTH) != expected.get(Calendar.MONTH)) {
+            return hackosPerMonth * (actual.get(Calendar.MONTH) - expected.get(Calendar.MONTH));
+        } else {
+            return hackosPerDay * (actual.get(Calendar.DAY_OF_MONTH) - expected.get(Calendar.DAY_OF_MONTH));
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        String sActual = in.nextLine();
+        String sExpected = in.nextLine();
+
+        System.out.println(calculateLibraryFine(sActual, sExpected));
+    }
+}
---
+++ warmup/src/test/java/com/hakerrank/mk/LibraryFineTest.java
@@ -0,0 +1,86 @@
+package com.hakerrank.mk;
+
+
+import com.hackerrank.mk.LibraryFine;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LibraryFineTest {
+
+    @Test
+    public void testZero() {
+        String expected = "10 10 2015";
+
+        String actual = "1 10 2015";
+        assertEquals(0, LibraryFine.calculateLibraryFine(actual, expected));
+
+        actual = "10 10 2015";
+        assertEquals(0, LibraryFine.calculateLibraryFine(actual, expected));
+
+        actual = "10 9 2015";
+        assertEquals(0, LibraryFine.calculateLibraryFine(actual, expected));
+
+        actual = "10 10 201";
+        assertEquals(0, LibraryFine.calculateLibraryFine(actual, expected));
+    }
+
+    @Test
+    public void testSameDay() {
+        String expected = "15 07 2015";
+
+        String actual = "15 07 2015";
+        assertEquals(0, LibraryFine.calculateLibraryFine(actual, expected));
+
+        actual = "15 06 2015";
+        assertEquals(0, LibraryFine.calculateLibraryFine(actual, expected));
+
+        actual = "15 08 2015";
+        assertEquals(LibraryFine.calculateLibraryFine(actual, expected), LibraryFine.calculateLibraryFine(actual, expected));
+
+        actual = "15 07 2014";
+        assertEquals(0, LibraryFine.calculateLibraryFine(actual, expected));
+
+        actual = "15 07 2016";
+        assertEquals(LibraryFine.hackosYear, LibraryFine.calculateLibraryFine(actual, expected));
+    }
+
+    @Test
+    public void testSameMonth() {
+        String expected = "1 10 2015";
+
+        String actual = "31 10 2015";
+        assertEquals(30 * LibraryFine.hackosPerDay, LibraryFine.calculateLibraryFine(actual, expected));
+
+        actual = "31 10 2014";
+        assertEquals(0, LibraryFine.calculateLibraryFine(actual, expected));
+
+        actual = "31 10 2016";
+        assertEquals(LibraryFine.hackosYear, LibraryFine.calculateLibraryFine(actual, expected));
+    }
+
+    @Test
+    public void testSameYear() {
+        String expected = "15 6 2015";
+
+        String actual = "22 12 2015";
+        assertEquals(6 * LibraryFine.hackosPerMonth, LibraryFine.calculateLibraryFine(actual, expected));
+
+        actual = "22 4 2015";
+        assertEquals(0, LibraryFine.calculateLibraryFine(actual, expected));
+
+        actual = "22 6 2015";
+        assertEquals(7 * LibraryFine.hackosPerDay, LibraryFine.calculateLibraryFine(actual, expected));
+    }
+
+    @Test
+    public void testDifferentYear() {
+        String expected = "1 2 2015";
+
+        String actual = "22 11 2016";
+        assertEquals(LibraryFine.hackosYear, LibraryFine.calculateLibraryFine(actual, expected));
+
+        actual = "31 12 2014";
+        assertEquals(0, LibraryFine.calculateLibraryFine(actual, expected));
+    }
+}
