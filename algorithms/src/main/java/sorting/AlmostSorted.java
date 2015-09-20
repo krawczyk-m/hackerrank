db19cab4bd4bd8e61c81bed9b0cbfd016aea79b3
Michał Krawczyk <krawczyk.michal91@gmail.com>
2015-09-20 22:58:28 +0200

    Implemented reverse sort for Almost Sorted challenge. Added tests for reverse sort

--- algorithms/src/main/java/sorting/AlmostSorted.java
+++ algorithms/src/main/java/sorting/AlmostSorted.java
@@ -4,10 +4,6 @@ import java.util.Scanner;
 
 public class AlmostSorted {
 
-  public static void print(Object o) {
-    System.out.println(o + "\n");
-  }
-
   public static String sortBySwap(int[] ar) {
     // variables for swap checks
     int toSwap = -1;
@@ -49,6 +45,43 @@ public class AlmostSorted {
     }
   }
 
+  public static String sortByReverse(int[] ar) {
+    int reverseStart = -1;
+    int reverseEnd = -1;
+    boolean canReverse = true;
+
+    int n = ar.length;
+    int i = 0;
+
+    while (++i < n && ar[i - 1] < ar[i]) {} // search for out-of-order value
+    if (i != n) { // found out-of-order value
+      reverseStart = i - 1;
+
+      while (++i < n && ar[i - 1] > ar[i]) {} // search of end of descending sequence
+      if (i != n) { // found end
+        reverseEnd = i - 1;
+
+        while (++i < n && ar[i - 1] < ar[i]) {} // search for another out-of-order value
+        if (i != n) { // out of order found - can not reverse
+          return "no";
+        }
+      } else {
+        reverseEnd = n - 1;
+      }
+    } else { // i == n -> in order
+      return "yes";
+    }
+
+    if (reverseStart != 0) canReverse = canReverse && ar[reverseStart - 1] < ar[reverseEnd];
+    if (reverseEnd != n - 1) canReverse = canReverse && ar[reverseStart] < ar[reverseEnd + 1];
+
+    if (canReverse) {
+      return "reverse " + (reverseStart + 1) + " " + (reverseEnd + 1);
+    } else {
+      return "no";
+    }
+  }
+
   public static void main(String[] args) {
     Scanner in = new Scanner(System.in);
 
@@ -60,50 +93,14 @@ public class AlmostSorted {
       ar[i] = in.nextInt();
     }
 
-    // variables for swap checks
-    int toSwap = -1;
-    int toSwap2 = -1;
-    boolean canSwap = true;
-
-    int i = 0;
-
-    String ret = "";
-
-    while(++i < n && ar[i - 1] < ar[i]) {} // search for out-of-order value
-    if (i != n) { // found out-of-order value
-      toSwap = i - 1;
-
-      while (++i < n && ar[i - 1] < ar[i]) {} // search for another out-of-order
-      if (i != n) { // found another out-of-order
-        toSwap2 = i;
-
-        while (++i < n && ar[i - 1] < ar[i]) {} // search for another out-of-order
-        if (i != n) { // found another out-of-order - cannot swap
-          ret = "no";
-        }
-      } else { // i == n
-        toSwap2 = toSwap + 1; // must swap the first out-of-order value found with the following one
-      }
-    } else { // i == n -> in order
-      ret = "yes";
-    }
-
-    if (toSwap < toSwap2 - 1) canSwap = canSwap && (ar[toSwap2 - 1] < ar[toSwap]);
-    if (toSwap2 < n - 1) canSwap = canSwap && (ar[toSwap] < ar[toSwap2 + 1]);
-
-    if (toSwap < toSwap2 - 1) canSwap = canSwap && (ar[toSwap2] < ar[toSwap + 1]);
-    if (toSwap != 0) canSwap = canSwap && (ar[toSwap - 1] < ar[toSwap2]);
-
-    if (canSwap) {
-      ret =  "swap " + (toSwap + 1) + " " + (toSwap2 + 1);
+    String swap = sortBySwap(ar);
+    String rev = sortByReverse(ar);
+    if (swap.contains("swap")) {
+      System.out.println("yes\n" + swap);
+    } else if (rev.contains("reverse")) {
+        System.out.println("yes\n" + rev);
     } else {
-      ret = "no";
+      System.out.println("no");
     }
-
-    if (canSwap) {
-      System.out.println("yes");
-    }
-    System.out.println(ret);
-
   }
 }
--- algorithms/src/test/java/sorting/AlmostSortedTest.java
+++ algorithms/src/test/java/sorting/AlmostSortedTest.java
@@ -37,5 +37,44 @@ public class AlmostSortedTest {
 
     int[] canNotSwap2 = {1, 9, 3, 4};
     assertEquals("no", AlmostSorted.sortBySwap(canNotSwap2));
+
+    int[] thirdToSwap = {1, 9, 3, 4, 2, 5, -1};
+    assertEquals("no", AlmostSorted.sortBySwap(thirdToSwap));
+  }
+
+  @Test
+  public void testReverse() {
+    int[] twoSorted = {2, 4};
+    assertEquals("yes", AlmostSorted.sortByReverse(twoSorted));
+
+    int[] sorted = {1, 2, 3, 4, 5, 6};
+    assertEquals("yes", AlmostSorted.sortByReverse(sorted));
+
+    int[] twoUnsorted = {4, 2};
+    assertEquals("reverse 1 2", AlmostSorted.sortByReverse(twoUnsorted));
+
+    int[] beginning = {2, 1, 3, 4, 5};
+    assertEquals("reverse 1 2", AlmostSorted.sortByReverse(beginning));
+
+    int[] end = {1, 2, 3, 4, 6, 5};
+    assertEquals("reverse 5 6", AlmostSorted.sortByReverse(end));
+
+    int[] middleClose = {10, 22, 44, 31, 56, 67};
+    assertEquals("reverse 3 4", AlmostSorted.sortByReverse(middleClose));
+
+    int[] random = {-10, 99, 90, 44, 20, -5, 404, 1001};
+    assertEquals("reverse 2 6", AlmostSorted.sortByReverse(random));
+
+    int[] all = {100, 55, 44, 33, 22, 11, -100};
+    assertEquals("reverse 1 7", AlmostSorted.sortByReverse(all));
+
+    int[] canNotReverse = {3, 1, 2};
+    assertEquals("no", AlmostSorted.sortByReverse(canNotReverse));
+
+    int[] canNotReverse2 = {1, 9, 3, 4};
+    assertEquals("no", AlmostSorted.sortByReverse(canNotReverse2));
+
+    int[] canNotReverse3 = {1, 10, 50, 40, 30, 20, 60, 55};
+    assertEquals("no", AlmostSorted.sortByReverse(canNotReverse3));
   }
 }
